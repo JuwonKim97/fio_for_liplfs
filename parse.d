535c92429ec34cf5c25d383bd3def0c555eac175
JuwonKim97 <joo97508@gmail.com>
2023-06-28 14:51:42 +0900

    init

---
+++ parse.d
@@ -0,0 +1,19 @@
+parse.o: parse.c config-host.h compiler/compiler.h parse.h flist.h \
+ debug.h lib/types.h log.h lib/output_buffer.h options.h optgroup.h \
+ minmax.h lib/ieee754.h lib/pow2.h lib/types.h y.tab.h
+parse.c:
+config-host.h:
+compiler/compiler.h:
+parse.h:
+flist.h:
+debug.h:
+lib/types.h:
+log.h:
+lib/output_buffer.h:
+options.h:
+optgroup.h:
+minmax.h:
+lib/ieee754.h:
+lib/pow2.h:
+lib/types.h:
+y.tab.h:
---
+++ t/io_uring.d
@@ -0,0 +1,114 @@
+t/io_uring.o: t/io_uring.c config-host.h t/../arch/arch.h \
+ t/../arch/../lib/types.h t/../arch/arch-x86_64.h \
+ t/../arch/arch-x86-common.h t/../arch/../lib/ffz.h t/../os/os.h \
+ t/../os/../arch/arch.h t/../os/../lib/types.h t/../os/os-linux.h \
+ t/../os/./os-linux-syscall.h t/../os/./../arch/arch.h t/../os/../file.h \
+ t/../os/../compiler/compiler.h t/../os/../io_ddir.h t/../os/../flist.h \
+ t/../os/../lib/zipf.h t/../os/../lib/rand.h t/../os/../lib/types.h \
+ t/../os/../lib/axmap.h t/../os/../lib/lfsr.h t/../os/../lib/gauss.h \
+ t/../os/../oslib/strlcat.h t/../lib/types.h t/../lib/roundup.h lib/fls.h \
+ t/../lib/rand.h t/../minmax.h t/../os/linux/io_uring.h \
+ t/../engines/nvme.h t/../engines/../fio.h \
+ t/../engines/../compiler/compiler.h t/../engines/../thread_options.h \
+ t/../engines/../arch/arch.h t/../engines/../os/os.h \
+ t/../engines/../options.h t/../engines/../parse.h \
+ t/../engines/../flist.h t/../engines/../lib/types.h \
+ t/../engines/../stat.h t/../engines/../iolog.h \
+ t/../engines/../lib/rbtree.h t/../engines/../lib/ieee754.h \
+ t/../engines/../ioengines.h t/../engines/../io_u.h \
+ t/../engines/../io_ddir.h t/../engines/../debug.h t/../engines/../file.h \
+ t/../engines/../workqueue.h t/../engines/../zbd_types.h \
+ t/../engines/../lib/output_buffer.h t/../engines/../diskutil.h \
+ t/../engines/../helper_thread.h t/../engines/../fio_sem.h \
+ t/../engines/../json.h t/../engines/../gettime.h \
+ t/../engines/../lib/seqlock.h t/../engines/../lib/types.h \
+ t/../engines/../lib/../arch/arch.h t/../engines/../lib/pattern.h \
+ t/../engines/../td_error.h t/../engines/../fifo.h t/../engines/../log.h \
+ t/../engines/../helpers.h t/../engines/../minmax.h \
+ t/../engines/../profile.h t/../engines/../fio_time.h \
+ t/../engines/../oslib/getopt.h t/../engines/../lib/rand.h \
+ t/../engines/../lib/num2str.h t/../engines/../lib/memalign.h \
+ t/../engines/../smalloc.h t/../engines/../client.h \
+ t/../engines/../server.h t/../engines/../flow.h \
+ t/../engines/../io_u_queue.h t/../engines/../steadystate.h \
+ t/../engines/../lib/nowarn_snprintf.h t/../engines/../dedupe.h
+t/io_uring.c:
+config-host.h:
+t/../arch/arch.h:
+t/../arch/../lib/types.h:
+t/../arch/arch-x86_64.h:
+t/../arch/arch-x86-common.h:
+t/../arch/../lib/ffz.h:
+t/../os/os.h:
+t/../os/../arch/arch.h:
+t/../os/../lib/types.h:
+t/../os/os-linux.h:
+t/../os/./os-linux-syscall.h:
+t/../os/./../arch/arch.h:
+t/../os/../file.h:
+t/../os/../compiler/compiler.h:
+t/../os/../io_ddir.h:
+t/../os/../flist.h:
+t/../os/../lib/zipf.h:
+t/../os/../lib/rand.h:
+t/../os/../lib/types.h:
+t/../os/../lib/axmap.h:
+t/../os/../lib/lfsr.h:
+t/../os/../lib/gauss.h:
+t/../os/../oslib/strlcat.h:
+t/../lib/types.h:
+t/../lib/roundup.h:
+lib/fls.h:
+t/../lib/rand.h:
+t/../minmax.h:
+t/../os/linux/io_uring.h:
+t/../engines/nvme.h:
+t/../engines/../fio.h:
+t/../engines/../compiler/compiler.h:
+t/../engines/../thread_options.h:
+t/../engines/../arch/arch.h:
+t/../engines/../os/os.h:
+t/../engines/../options.h:
+t/../engines/../parse.h:
+t/../engines/../flist.h:
+t/../engines/../lib/types.h:
+t/../engines/../stat.h:
+t/../engines/../iolog.h:
+t/../engines/../lib/rbtree.h:
+t/../engines/../lib/ieee754.h:
+t/../engines/../ioengines.h:
+t/../engines/../io_u.h:
+t/../engines/../io_ddir.h:
+t/../engines/../debug.h:
+t/../engines/../file.h:
+t/../engines/../workqueue.h:
+t/../engines/../zbd_types.h:
+t/../engines/../lib/output_buffer.h:
+t/../engines/../diskutil.h:
+t/../engines/../helper_thread.h:
+t/../engines/../fio_sem.h:
+t/../engines/../json.h:
+t/../engines/../gettime.h:
+t/../engines/../lib/seqlock.h:
+t/../engines/../lib/types.h:
+t/../engines/../lib/../arch/arch.h:
+t/../engines/../lib/pattern.h:
+t/../engines/../td_error.h:
+t/../engines/../fifo.h:
+t/../engines/../log.h:
+t/../engines/../helpers.h:
+t/../engines/../minmax.h:
+t/../engines/../profile.h:
+t/../engines/../fio_time.h:
+t/../engines/../oslib/getopt.h:
+t/../engines/../lib/rand.h:
+t/../engines/../lib/num2str.h:
+t/../engines/../lib/memalign.h:
+t/../engines/../smalloc.h:
+t/../engines/../client.h:
+t/../engines/../server.h:
+t/../engines/../flow.h:
+t/../engines/../io_u_queue.h:
+t/../engines/../steadystate.h:
+t/../engines/../lib/nowarn_snprintf.h:
+t/../engines/../dedupe.h:
